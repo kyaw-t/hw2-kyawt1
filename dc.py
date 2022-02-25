f4d11891a895ad9fcfa810ba96d6975c17ad5475
kyaw-t <90881094+kyaw-t@users.noreply.github.com>
2022-02-24 23:12:15 -0500

    initial commit

--- dc.py
+++ dc.py
@@ -23,6 +23,7 @@ for line in gzip.open(dict_file, 'rt'):
     dictionary[word] = n
 
 class DC(search.Problem):
+
     """DC is a subclass of the AIMA search files's Problem class
        It's init method takes three arguments: the initial word, goal word and cost method.
 
@@ -32,12 +33,45 @@ class DC(search.Problem):
        dictionary. The cost argument specifies how to measure the
        cost of an action and can be 'steps', 'scrabble' or 'frequency'
        """
+    alphabet = {
+        'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 
+        'h': 4, 'i': 1, 'j': 6, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 
+        'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 
+        'v': 4, 'w': 4, 'x': 6, 'y': 4, 'z':10
+    }
 
     def __init__(self, initial='dog', goal='cat', cost='steps'):
+
         # set instance attributes ...
-        pass
+        self.initial = initial
+        self.goal = goal
+        self.cost_method = cost
+        self.current_cost = 0
+        self.dictionary = dictionary
+        
         # make sure the arguments are legal, raising an error if any are bad.
-        pass
+        for var in  [initial, goal, cost]:
+            if type(var) != str:
+                raise TypeError("All arguments must be a string")
+
+        for string in [initial, goal]:
+            if len(string) != 3 and len(string) != 4:
+                raise ValueError("%s must be a string of length 3 or 4." % string)
+
+            elif string not in self.dictionary:
+                raise ValueError("%s must be a string of length 3 or 4." % string)
+
+        if len(initial) != len(goal):
+            raise ValueError("%s and %s must be strings of equal length." % (initial, goal))
+            
+
+        if cost not in ["steps", "scrabble", "frequency"]:
+            raise ValueError("%s is an invalid cost method" % cost)
+
+        # if the cost method is frequency calls a helper function to calculate heuristic
+        if self.cost_method == "frequency":
+            self.frequency_h()
+
 
     def actions(self, state):
         """ Given a state (i.e., a word), return a list or iterator of
@@ -47,27 +81,63 @@ class DC(search.Problem):
         should not be the same as the state, i.e., don't replace a
         character with the same character """
 
-        pass
+        valid_actions = []
+        
+        # iterates through the word and tries replacing each character
+        # with every letter from the alphabet to check for valid words
+        for i in range(len(state)):
+            for letter in self.alphabet:
+
+                temp = self.result(state, (i, letter))
+                if temp in self.dictionary and temp != state:
+                    valid_actions.append((i, temp[i]))
+        
+        return valid_actions
 
     def result(self, state, action):
         """ takes a state and an action and returns a new state """
-        pass
+        idx = action[0]
+        return state[:idx] + action[1] + state[idx+1:]            
 
     def goal_test(self, state):
-        """ returns True iff state is a goal state for this problem instance """
-        pass
+        return state == self.goal
 
+    
     def path_cost(self, c, state1, action, state2):
         """ Returns the cost to get to state2 by applying action in
         state1 given that c is the cost to get from the state state to
         state1. For the the dc problem, you will have to check what
         the cost metric is being used for this problem instance, i.e.,
         is it steps, scrabble or frequency """
-        pass
+        if self.cost_method == "steps":
+            return c + 1
+                
+        elif self.cost_method == "scrabble":
+            return c + self.alphabet[action[1]]
+
+        elif self.cost_method == "frequency":
+            return c + 1 + dictionary[state2]
 
     def __repr__(self):
         """" return a suitable string to represent this problem instance """
-        pass
+        return ("dc(%s, %s, %s)" % (self.initial, self.goal, self.cost_method))
+
+    def frequency_h(self):
+        """" helper function for calculating heuristcs if the cost method 
+        is frequency calculates the minimum possible cost of a word 
+        that has the same letter as the goal word at a given position """
+
+        # an array of length = len(word) populated with 9999
+        self.frequency_costs = [9999 for i in range(len(self.goal))] 
+
+        # iterates through dictionary, calculating min possible cost 
+        # of words sharing the same letter in the same poisition as the goal word
+        for word in dictionary:
+            if len(word) == len(self.goal):
+                for i in range(len(self.goal)):
+                    if self.goal[i] == word[i] and dictionary[word] < self.frequency_costs[i]:
+                        self.frequency_costs[i] = dictionary[word]
+
 
     def h(self, node):
         """Heuristic: returns an estimate of the cost to get from the
@@ -75,5 +145,40 @@ class DC(search.Problem):
         should depend on the Problem's cost parameter (steps, scrabble
         or frequency) as this will effect the estimate cost to get to
         the nearest goal. """
-        pass
+
+        estimate = 0
+
+        # every difference in chracter adds 1 to the estimate
+        if self.cost_method == "steps":
+            for i in range(len(node.state)):
+                if node.state[i] != self.goal[i]:
+                    estimate += 1
+
+        # every difference in character adds an amount based 
+        # on the desired character's scrabble cost
+        elif self.cost_method == "scrabble":
+            for i in range(len(node.state)):
+                if node.state[i] != self.goal[i]:
+                    estimate += self.alphabet[self.goal[i]]
+        
+        # every difference in chracter adds the lowest possible
+        # cost of a word that has the desired character at
+        # the given position
+        elif self.cost_method == "frequency":
+
+            arr = []
+            for i in range(len(node.state)):
+                if node.state[i] != self.goal[i]:
+                    arr.append(1 + self.frequency_costs[i])
+            
+            if len(arr) == 1:
+                estimate = 1 + dictionary[self.goal]
+            else:
+                estimate = sum(arr) 
+
+        return estimate
+
+
+
+        
 
--- questions.md
+++ questions.md
@@ -5,20 +5,29 @@ Please answer the following questions using the git [markdown syntax](https://gu
 
 ### (1) Describe in words the heuristic you used for the steps cost and explain why it is admissable
 
-... your answer here ...
+For the steps cost, the heuristic counts the number of letters of the given word that does not match the letters in the goal word. For each letter that does not match, a cost of 1 is added to our estimate. 
+This heuristic is admissable because this is the minimum amount of changes that need to be done in order to attain the goal word.
 
 ### (2) Describe in words the heuristic you used for the scrabble cost and explain why it is admissable
 
-... your answer here ...
-
+For the scrabble cost, the heuristic checks which letters of the goal word do not match the letters of the given word. For each letter of the goal word that does not match, the scrabble cost of that letter is added to our estimate.
+This heuristic is admissable because these letters must eventually be used to attain the goal word. When these letters are used their scrabble costs are added to the total cost so out heurisitc is simply calculating the minimum cost required to get to the goal word.
 ### (3) Describe in words the heuristic you used for the frequency cost and explain why it is admissable
 
-... your answer here ...
+For the frequency cost, the heuristic checks which letters of the goal word do not match the letters of the given word. For each letter of the goal word that does not match, the lowest possible cost of a word with that letter at that given position + 1 is added to our estimate. 
+This heuristic is admissable because for every mismatched letter, a word with that letter at that position must be used to attain the goal word. So by adding the minimum possible cost of using such a word to our estimate, the heuristic always estimates a lower or equal cost.
+
 
 ### (4) Given an intiial word W1 and goal words W2, if there is a shortest path with N steps from W1 to W2, will there also be a shortest path of N steps from W2 to W1?  Explain why or why not.
 
-... your answer here ...
+Yes, because given a path going from W1 to W2 we could take the reverse path to go from W2 to W1. We can take the reverse path because any legal word that is transformed into another legal word can be transformed back into the same original word.
 
 ### (5) Using the steps cost, what is the longest path for a pair of three- and four-letter words you can find?
 
-... your answer here ...
+The longest path for three-letter words I could find was between wag and oxy which required 9 steps.
+dc(wow, oxo, steps) cost:7.00; time:0.917; solution:wow wot oot ort ore one ono oxo; deltas:[-4, -3, -3, -2, -1, 0.0, 0.0]
+
+And the longest path for four-letter words I could find was between unco and arum which required 14 steps. 
+dc(unco, arum, steps) cost:13.00; time:0.831; solution:unco unto into ingo ings inks irks iris cris crim cram dram drum arum;
+
+I searched for the longest path by creating a simple python program that tried the dog cat game with over ten thousand different pairs of words. The program then took the longest pairs it could find and tried extending the pairs by changing the words with words in their respective actions list.
